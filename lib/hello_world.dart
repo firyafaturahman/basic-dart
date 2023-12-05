af5153d62ecc922efe8e156b35b9883dbdd49b00
firyafaturahman <firya.faturahman2017@gmail.com>
2023-12-05 21:12:55 +0700

    basic dart

---
+++ lib/hello_world.dart
@@ -0,0 +1,27 @@
+main() {
+  // 1. Basic Dart Program For Printing Name
+  // var name = "Firya Faturahman";
+  // print(name);
+
+  // 2. Basic Dart Program To Join One Or More Variables
+  // var firstName = "Firya";
+  // var lastName = "Faturahman";
+  // print("Nama saya adalah $firstName $lastName");
+
+  // 3.Dart Program For Basic Calculation
+  // Performing addition, subtraction, multiplication, and division in dart.
+
+  var number1 = 10;
+  var number2 = 3;
+
+  // calculation
+  int jumlah = number1 + number2;
+  int kurang = number1 - number2;
+  int kali = number1 * number2;
+  int bagi = number1 ~/ number2;
+
+  print("penjumlahan : $jumlah");
+  print("pengurangan : $kurang");
+  print("perkalian : $kali");
+  print("pembagian : $bagi");
+}
