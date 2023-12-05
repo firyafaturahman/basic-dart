d411f3bd245b8d158a49554df6a84b6f0324d755
firyafaturahman <firya.faturahman2017@gmail.com>
2023-12-05 21:53:50 +0700

    data types

---
+++ lib/data_types.dart
@@ -0,0 +1,25 @@
+main() {
+// Numbers
+  double number1 = 23.6;
+  int number2 = 22;
+
+  print("penjumlahan: ${number1 + number2}");
+  print("pengurangan: ${number1 - number2}");
+  print("perkalian: ${number1 * number2}");
+  print("pembagian: ${number1 / number2}");
+
+// Strings
+  String fullName = "Firya Faturahman";
+  print("Nama saya adalah : $fullName");
+// Booleans
+
+  bool isMarried = false;
+  isMarried == true
+      ? print("Status is married")
+      : print("Status isn't married");
+// Lists
+// Maps
+// Sets
+// Runes
+// Null
+}
