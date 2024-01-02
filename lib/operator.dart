20ff91bc0c3385beec1d0aa227a27d1b9ea24216
firyafaturahman <firya.faturahman2017@gmail.com>
2024-01-02 21:45:01 +0700

    opeartor

---
+++ lib/operator.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int number1 = 8;
+  int number2 = 4;
+
+  print("penjumlahan: ${number1 + number2}");
+  print("pengurangaan: ${number1 - number2}");
+  print("perkalian: ${number1 * number2}");
+  print("pembagian: ${number1 / number2}");
+  print("pembagian: ${number1 ~/ number2}");
+
+  print("increment: ${number1++}");
+  print("decrement: ${number1--}");
+  print("decrement: ${++number2}");
+  print("decrement: ${--number2}");
+}
